c56ca8a1c5b617dbf83675514bc2c2a51c31afcb
Sergey <go.for.broke1006@gmail.com>
2017-11-15 20:23:51 +0300

    Update install-go-1.9.2.sh

--- .docker/bin/install-go-1.9.2.sh
+++ .docker/bin/install-go-1.9.2.sh
@@ -12,3 +12,16 @@ echo "export GOPATH=~/go" >> ~/.profile
 echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
 
 source ~/.profile
+
+go get -u github.com/golang/dep/cmd/dep
+go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+
+cd ~/
+curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
+rm -rf protoc3
+unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
+rm -f protoc-3.2.0-linux-x86_64.zip
+sudo mv protoc3/bin/* /usr/local/bin/
+sudo mv protoc3/include/* /usr/local/include/
+sudo ln -s ~/protoc3/bin/protoc /usr/bin/protoc
+
