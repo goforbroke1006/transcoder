3568b5835c1e9dd98bd11d5480899c1d52100b44
Sergey <go.for.broke1006@gmail.com>
2017-11-15 17:59:27 +0300

    Update install-go-1.9.2.sh

--- .docker/bin/install-go-1.9.2.sh
+++ .docker/bin/install-go-1.9.2.sh
@@ -4,8 +4,8 @@ apt-get install -y wget
 
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
 tar -C /usr/local -xvzf go1.9.2.linux-amd64.tar.gz
-chmod -R 0777 /usr/local/go/
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export PATH=$PATH:$GOROOT/bin" >> ~/.profile
 
 mkdir -p ~/go/{bin,pkg,src}
 echo "export GOPATH=~/go" >> ~/.profile
