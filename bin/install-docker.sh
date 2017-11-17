f01237f6b3a1af1022629656e0155fd9a53ac594
Sergey <go.for.broke1006@gmail.com>
2017-11-17 12:31:21 +0300

    Create install-docker.sh

---
+++ bin/install-docker.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+### SET UP THE REPOSITORY
+
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+#sudo add-apt-repository \
+#   "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+#sudo add-apt-repository \
+#   "deb [arch=s390x] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+
+
+### INSTALL DOCKER CE
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+### RUN WITHOUT SUDO
+sudo usermod -aG docker $USER
+sudo chmod -R 0777 ~/.docker/
