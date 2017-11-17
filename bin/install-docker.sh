26669d8dfa0dda871f0b35574a8f30774579fb9e
Sergey <go.for.broke1006@gmail.com>
2017-11-17 17:15:22 +0300

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 ### SET UP THE REPOSITORY
-
 sudo apt-get update
 sudo apt-get -y install \
     apt-transport-https \
@@ -10,7 +9,6 @@ sudo apt-get -y install \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -23,7 +21,6 @@ sudo add-apt-repository \
 #   "deb [arch=s390x] https://download.docker.com/linux/ubuntu \
 #   $(lsb_release -cs) \
 #   stable"
-
 
 ### INSTALL DOCKER CE
 sudo apt-get update
