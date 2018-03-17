5b5289c93f094b5381f17a47a8b7309b2fbe3e63
Sergey <go.for.broke1006@gmail.com>
2018-03-17 17:07:17 +0300

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,40 +1,27 @@
 #!/usr/bin/env bash
 
-### SET UP THE REPOSITORY
+# SET UP THE REPOSITORY
 sudo apt-get update
-#sudo apt-get -y install \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo apt-key fingerprint 0EBFCD88
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo add-apt-repository "deb [arch=s390x] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian jessie stable"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
+# INSTALL DOCKER CE
 sudo apt-get update
-
-sudo apt-get install docker-ce
-
-### INSTALL DOCKER CE
-sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-ce docker-compose
 
 ### RUN WITHOUT SUDO
 sudo usermod -aG docker $USER
 sudo chmod -R 0777 ~/.docker/
 sudo chmod -R 0777 /var/run/docker.sock
-
-### INSTALL DOCKER-COMPOSE
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod 0777 /usr/local/bin/docker-compose
+
+sudo docker run hello-world
